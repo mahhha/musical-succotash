9dcba01c6f8d8909e9676af41a0261bc57669142
mahhha <79687513+mahhha@users.noreply.github.com>
2021-04-16 18:29:14 +0530

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,142 @@
+package com.icf.assignment.springboot.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="employees")
+public class Employee {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(name = "first_name")
+	private String firstName;
+	
+	@Column(name = "last_name")
+	private String lastName;
+	
+	@Column(name = "employment_id")
+	private String employmentId;
+	
+	@Column(name = "start_date")
+	private String startDate;
+	
+	@Column(name = "end_date")
+	private String endDate;
+	
+	@Column(name = "designation")
+	private String designation;
+	
+	@Column(name = "department")
+	private String department;
+	
+	@Column(name = "status")
+	private String status;
+	
+	@Column(name = "dob")
+	private String dob;
+	
+	@Column(name = "reporting_manager")
+	private String reportingManager;
+	
+	@Column(name = "gender")
+	private String gender;
+	
+	@Column(name = "blood_group")
+	private String bloodGroup;
+	
+	@Column(name = "address")
+	private String address;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmploymentId() {
+		return employmentId;
+	}
+	public void setEmploymentId(String employmentId) {
+		this.employmentId = employmentId;
+	}
+	public String getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(String startDate) {
+		this.startDate = startDate;
+	}
+	public String getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(String endDate) {
+		this.endDate = endDate;
+	}
+	public String getDesignation() {
+		return designation;
+	}
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getDob() {
+		return dob;
+	}
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+	public String getReportingManager() {
+		return reportingManager;
+	}
+	public void setReportingManager(String reportingManager) {
+		this.reportingManager = reportingManager;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getBloodGroup() {
+		return bloodGroup;
+	}
+	public void setBloodGroup(String bloodGroup) {
+		this.bloodGroup = bloodGroup;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+
+}
